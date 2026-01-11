266e012cea36d6614bb2ee1b37f262398a15c741
abhgarg1984 <abhishek.hi@hotmail.com>
2026-01-11 13:10:11 +0530

    Committing 8 items from workspace 31f9cbf4-392e-41c7-aa92-cff7786209b2

---
+++ Workspace-01/Notebook 2.Notebook/notebook-content.sql
@@ -0,0 +1,33 @@
+-- Fabric notebook source
+
+-- METADATA ********************
+
+-- META {
+-- META   "kernel_info": {
+-- META     "name": "sqldatawarehouse"
+-- META   },
+-- META   "dependencies": {
+-- META     "warehouse": {
+-- META       "default_warehouse": "cfd52e39-64d5-baf2-4646-bfda8fa1759f",
+-- META       "known_warehouses": [
+-- META         {
+-- META           "id": "cfd52e39-64d5-baf2-4646-bfda8fa1759f",
+-- META           "type": "Datawarehouse"
+-- META         }
+-- META       ]
+-- META     }
+-- META   }
+-- META }
+
+-- CELL ********************
+
+-- Welcome to your new notebook
+-- Type here in the cell editor to add code!
+
+
+-- METADATA ********************
+
+-- META {
+-- META   "language": "sql",
+-- META   "language_group": "sqldatawarehouse"
+-- META }
